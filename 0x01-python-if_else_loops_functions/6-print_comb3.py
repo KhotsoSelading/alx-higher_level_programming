d81ae6eed0bbbf2eb5f920b5b00222f3fa4c2d49
KhotsoSelading <k.selading007@gmail.com>
2023-06-06 19:56:39 +0200

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-digits = [str(digit1) + str(digit2)
-          for digit1 in range(9) for digit2 in range(digit1 + 1, 10)]
-print(", ".join(digits))
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        print("{:d}{:d}".format(digit1, digit2), end=', ')
+print("89")

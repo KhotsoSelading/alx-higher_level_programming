e17911e9c37f6929daac270da3b6bbdd8dcdbf0f
KhotsoSelading <k.selading007@gmail.com>
2023-06-06 20:51:10 +0200

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    output = ""
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            output += "FizzBuzz "
-        elif i % 3 == 0:
-            output += "Fizz "
-        elif i % 5 == 0:
-            output += "Buzz "
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{} ".format(i), end="")
+            print("{} ".format(number), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-	converted_str = ""
-	for char in str:
-		if "a" <= char <= "z":
-			converted_str += chr(ord(char) - 32)
-		else:
-			converted_str += char
-	print(converted_str)
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print(uppercase_char, end='')
+    print()

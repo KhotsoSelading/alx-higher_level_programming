7d4cbc1d13a00bbcca7e7c037cb810bb2c2afb38
KhotsoSelading <k.selading007@gmail.com>
2023-06-06 19:35:21 +0200

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-	if i not in (101, 113):
-		print(chr(i), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if i not in (ord('e'), ord('q')):
+        print(chr(i), end='')

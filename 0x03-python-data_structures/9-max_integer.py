e6f1b96353e7171708ded277dc1de91fc6b7e3b4
KhotsoSelading <k.selading007@gmail.com>
2023-06-12 17:17:38 +0200

    0x03-python-data_structures - Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def imax_integer(my_list=[]):
+def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return (None)
+        return "None"
     else:
-        imax = my_list[0]
+        max_num = my_list[0]
         for x in range(len(my_list)):
-            if my_list[x] > imax:
-                imax = my_list[x]
-        return (imax)
+            if my_list[x] > max_num:
+                max_num = my_list[x]
+        return (max_num)

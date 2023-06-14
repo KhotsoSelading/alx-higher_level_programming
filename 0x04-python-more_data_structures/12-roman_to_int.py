fb3065c4483fd50530b3bdd41a2a965bf1c78cfc
KhotsoSelading <k.selading007@gmail.com>
2023-06-14 18:36:43 +0200

    #0x04 All tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,49 +1,32 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    sub_i = 0
-    max_list = max(list_num)
-
-    i = 0
-    while i < len(list_num):
-        n = list_num[i]
-        if n < max_list:
-            sub_i += n
-        i += 1
-
-    return max_list - sub_i
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return 0
 
-    if not isinstance(roman_string, str):
-        return 0
-
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(roman_dict.keys())
-
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
     num = 0
-    prev_value = 0
-    list_num = [0]
 
     i = 0
     while i < len(roman_string):
-        ch = roman_string[i]
-        j = 0
-        while j < len(list_keys):
-            r_num = list_keys[j]
-            if r_num == ch:
-                if roman_dict.get(ch) <= prev_value:
-                    num += to_subtract(list_num)
-                    list_num = [roman_dict.get(ch)]
-                else:
-                    list_num.append(roman_dict.get(ch))
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return 0
 
-                prev_value = roman_dict.get(ch)
-            j += 1
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
+        else:
+            num += roman_dict[roman_string[i]]
+
         i += 1
 
-    num += to_subtract(list_num)
-
-    return num
+    return (num)

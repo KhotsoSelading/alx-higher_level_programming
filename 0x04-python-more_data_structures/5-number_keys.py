bbeac30df326732ea167fe3dbd5664dc1c2c5f24
KhotsoSelading <k.selading007@gmail.com>
2023-06-14 18:12:49 +0200

    # 0x04 All tasks

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    list_keys = list(a_dictionary.keys())
+    index_num = 0
+
+    for x in list_keys:
+        index_num += 1
+
+    return (index_num)

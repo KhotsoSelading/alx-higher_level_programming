39a3aaf095478df0e4dcc273149b77f65da7b454
KhotsoSelading <k.selading007@gmail.com>
2023-06-14 18:32:49 +0200

    #0x04 All tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    sub_i = 0
+    max_list = max(list_num)
+
+    i = 0
+    while i < len(list_num):
+        n = list_num[i]
+        if n < max_list:
+            sub_i += n
+        i += 1
+
+    return max_list - sub_i
+
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
         return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    list_keys = list(roman_dict.keys())
+
+    num = 0
     prev_value = 0
-    i = len(roman_string) - 1
+    list_num = [0]
 
-    while i >= 0:
-        char = roman_string[i]
-        current_value = roman_dict[char]
+    i = 0
+    while i < len(roman_string):
+        ch = roman_string[i]
+        j = 0
+        while j < len(list_keys):
+            r_num = list_keys[j]
+            if r_num == ch:
+                if roman_dict.get(ch) <= prev_value:
+                    num += to_subtract(list_num)
+                    list_num = [roman_dict.get(ch)]
+                else:
+                    list_num.append(roman_dict.get(ch))
 
-        if current_value < prev_value:
-            result -= current_value
-        else:
-            result += current_value
+                prev_value = roman_dict.get(ch)
+            j += 1
+        i += 1
 
-        prev_value = current_value
-        i -= 1
+    num += to_subtract(list_num)
 
-    return result
+    return num

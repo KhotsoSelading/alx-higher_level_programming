c53cec6ff56e075681b2567cc8d3c8c7ff8717e5
KhotsoSelading <k.selading007@gmail.com>
2023-06-27 12:47:15 +0200

    0x06. Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
+
+
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0):
-        """
-        __init__ - Initialize a new square.
+        """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
+
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """
-        __init__ - Initialize a new square.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.

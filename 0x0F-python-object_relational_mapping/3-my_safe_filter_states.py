d6267120eb4d5c8ff7a964d4ca27a01615e65c5b
KhotsoSelading <k.selading007@gmail.com>
2023-09-22 14:37:53 +0200

    Task 3 Changed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db_conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                              user=argv[1], passwd=argv[2], db=argv[3])
+    db_conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     db_cur = db_conn.cursor()
-    db_cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                   .format(argv[4]))
+    db_cur.execute("SELECT * FROM states WHERE name = %s;", (argv[4],))
     rows = db_cur.fetchall()
     for row in rows:
         print(row)

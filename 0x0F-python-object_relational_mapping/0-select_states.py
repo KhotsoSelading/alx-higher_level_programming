c2abfeec8fc32ac7377dbc114136b9a34c73b024
KhotsoSelading <k.selading007@gmail.com>
2023-09-22 13:34:45 +0200

    All 0x0F Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 """Lists of all states"""
-
 import MySQLdb
-from sys import argv
+import sys
+
+
+def list_states(username, password, database_name):
+    try:
+        db_conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                  passwd=password, db=database_name)
+        cursor = db_conn.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        if db_conn:
+            db_conn.close()
+
 
 if __name__ == "__main__":
-    db_conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                              user=argv[1], passwd=argv[2], db=argv[3])
-    db_cur = db_conn.cursor()
-    db_cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = db_cur.fetchall()
-    for row in query_rows:
-        print(row)
-    db_cur.close()
-    db_conn.close()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    list_states(username, password, database_name)

8307f7ceaa22e56070479851821b1fd25bd19e2a
KhotsoSelading <k.selading007@gmail.com>
2023-10-24 12:44:41 +0200

    0x14

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,24 +1,8 @@
 #!/usr/bin/node
-
 /**
- * A script that displays the status code of a GET request using axios.
+ * A script that display the status code of a GET request.
  */
-const axios = require('axios');
-
-const url = process.argv[2];
-
-if (url) {
-  axios.get(url)
-    .then(response => {
-      console.log(`code: ${response.status}`);
-    })
-    .catch(error => {
-      if (error.response) {
-        console.log(`code: ${error.response.status}`);
-      } else {
-        console.error('Error:', error.message);
-      }
-    });
-} else {
-  console.error('Usage: node script.js <URL>');
-}
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
